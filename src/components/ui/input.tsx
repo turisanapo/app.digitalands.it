9326aabfb35ef7224c796ae3fea4056928c502b4
DavideSelvaggio <selvaggiodavide@hotmail.it>
2024-09-22 12:47:22 +0800

    Remove empty input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
 import { cn } from "@/src/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+// Remove the empty interface
+// export interface InputProps
+//   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+// Use the React.InputHTMLAttributes<HTMLInputElement> directly
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
